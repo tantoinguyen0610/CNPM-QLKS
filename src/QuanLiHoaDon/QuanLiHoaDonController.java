af1be3b09b53ed893ee1ef765055c10ca43eee14
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-09 14:28:23 +0700

    Quản Lí Hóa Đơn Coding

---
+++ src/QuanLiHoaDon/QuanLiHoaDonController.java
@@ -0,0 +1,100 @@
+package QuanLiHoaDon;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class QuanLiHoaDonController {
+
+    @FXML
+    private Button XoaHoaDonThanhToanPhongButton;
+
+    @FXML
+    private Button LuuHoaDonThanhToanPhongButton;
+
+    @FXML
+    private Button ThoatHoaDonThanhToanPhongButton;
+
+    @FXML
+    private Button XoaHoaDonSuaChuaPhongButton;
+
+    @FXML
+    private Button LuuHoaDonSuaChuaPhongButton;
+
+    @FXML
+    private Button DuyetHoaDonSuaChuaPhongButton;
+
+    @FXML
+    private Button ThoatHoaDonSuaChuaPhongPhongButton;
+
+    @FXML
+    private Button XoaHoaDonNangCapPhongButton;
+
+    @FXML
+    private Button ThoatHoaDonNangCapPhongButton;
+
+    @FXML
+    private Button DuyetHoaDonNangCapPhongButton;
+
+    @FXML
+    private Button LuuHoaDonNangCapPhongButton;
+
+    @FXML
+    void DuyetHoaDonNangCapPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DuyetHoaDonSuaChuaPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuHoaDonNangCapPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuHoaDonSuaChuaPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuHoaDonThanhToanPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ThoatHoaDonNangCapPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ThoatHoaDonSuaChuaPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ThoatHoaDonThanhToanPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void XoaHoaDonNangCapPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void XoaHoaDonSuaChuaPhongButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void XoaHoaDonThanhToanPhongButtonListener(ActionEvent event) {
+
+    }
+
+}
