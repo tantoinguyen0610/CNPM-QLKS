b9222a99f39e9777c4d0bf802948e146406f69a5
builehongthang <blht01218332420@gmail.com>
2021-03-09 16:49:31 +0700

    giaodien

--- src/TraCuuPhong/GiaoDienPhong.java
+++ src/TraCuuPhong/GiaoDienPhong.java
@@ -1,4 +1,4 @@
-package HongThang;
+package TraCuuPhong;
 
 import javafx.application.Application;
 import javafx.fxml.*;
--- src/TraCuuPhong/GiaoDienTraCuuPhongController.java
+++ src/TraCuuPhong/GiaoDienTraCuuPhongController.java
@@ -1,4 +1,4 @@
-package HongThang;
+package TraCuuPhong;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
