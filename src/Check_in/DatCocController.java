0e9c912d22d3d43e7db2d77e26e4c744aec447be
undead4569 <pth.a3.30.hungphu@gmail.com>
2021-03-10 08:44:39 +0700

    cap nhat giao dien DatCoc

---
+++ src/Check_in/DatCocController.java
@@ -0,0 +1,39 @@
+package Check_in;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DatCocController {
+
+    @FXML
+    private Label DatCoc_Label;
+
+    @FXML
+    private Label SoTienDatCOc_Label;
+
+    @FXML
+    private Label SoTienKhachDC_Label;
+
+    @FXML
+    private Label NgayDC_Label;
+
+    @FXML
+    private Button Button_XacNhan;
+
+    @FXML
+    private Button Button_Huy;
+
+    @FXML
+    private TextField datCoc_textField;
+
+    @FXML
+    private TextField SoTienKhachDatCoc_textField;
+
+    @FXML
+    private DatePicker NgayDatCoc_textField;
+
+}
+
