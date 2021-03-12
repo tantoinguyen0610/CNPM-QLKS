abaec1ad6125a5c58974a9ebb0be660db86e2694
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-13 01:33:05 +0700

    Giao Diện Thêm Dịch Vụ Ăn Uống (file java + java Controller,fxml)

---
+++ src/QuanLiDichVu/ThemDichVuAnUongController.java
@@ -0,0 +1,105 @@
+package QuanLiDichVu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ThemDichVuAnUongController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label ThemDVAnUongLabel;
+
+    @FXML
+    private Label MaDVAnUongLabel;
+
+    @FXML
+    private Label TenDVAnUongLabel;
+
+    @FXML
+    private Label LoaiDVAnUongLabel;
+
+    @FXML
+    private Label SoLuongLabel;
+
+    @FXML
+    private Label GiaLabel;
+
+    @FXML
+    private Label TinhTrangLLabel;
+
+    @FXML
+    private TextField MaDVAnUongTextField;
+
+    @FXML
+    private TextField TenDVAnUongTextField;
+
+    @FXML
+    private TextField LoaiDVAnUongTextField;
+
+    @FXML
+    private TextField SoLuongTextField;
+
+    @FXML
+    private TextField GiaTextField;
+
+    @FXML
+    private TextField TinhTrangTextField;
+
+    @FXML
+    private Button LuuButton;
+
+    @FXML
+    private Button HuyButton;
+
+    @FXML
+    void GiaTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HuyButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LoaiDVAnUongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MaDVAnUongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SoLuongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TenDVAnUongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TinhTrangTextFieldListener(ActionEvent event) {
+
+    }
+
+   
+}
