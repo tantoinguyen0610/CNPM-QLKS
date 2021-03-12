572ef24089860c1efd4eb4b2c338a2edc2cf80b5
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-13 00:11:56 +0700

    Giao Diện Phiếu Dịch Vụ (file java + java Controller,fxml)

---
+++ src/QuanLiKhachHang/PhieuDichVuController.java
@@ -0,0 +1,237 @@
+package QuanLiKhachHang;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PhieuDichVuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label PhieuDichVuLabel;
+
+    @FXML
+    private Label DichVuAnUongLabel;
+
+    @FXML
+    private Label DichVuThuGianLabel;
+
+    @FXML
+    private Label DichVuGiaiTriLabel;
+
+    @FXML
+    private Label DichVuPhucVuLabel;
+
+    @FXML
+    private CheckBox SevenUpCheckBox;
+
+    @FXML
+    private CheckBox CocaCheckBox;
+
+    @FXML
+    private CheckBox BuffeeSangCheckBox;
+
+    @FXML
+    private CheckBox BuffeeTruaCheckBox;
+
+    @FXML
+    private CheckBox BuffeeToiCheckBox;
+
+    @FXML
+    private CheckBox BuffeeChayCheckBox;
+
+    @FXML
+    private CheckBox ComboBuffeeCaNgayCheckBox;
+
+    @FXML
+    private CheckBox PepsiCheckBox;
+
+    @FXML
+    private CheckBox NuocSuoiCheckBox;
+
+    @FXML
+    private CheckBox SpaCheckBox;
+
+    @FXML
+    private CheckBox ChamSocTocCheckBox;
+
+    @FXML
+    private CheckBox MassageCheckBox;
+
+    @FXML
+    private CheckBox XongHoiCheckBox;
+
+    @FXML
+    private CheckBox PhongGymCheckBox;
+
+    @FXML
+    private CheckBox QuayBarCheckBox;
+
+    @FXML
+    private CheckBox SanBongCheckBox;
+
+    @FXML
+    private CheckBox HoBoiCheckBox;
+
+    @FXML
+    private CheckBox PhongGameCheckBox;
+
+    @FXML
+    private CheckBox DatVeMayBayCheckBox;
+
+    @FXML
+    private CheckBox DatXeTaxiCheckBox;
+
+    @FXML
+    private CheckBox RuaXeCheckBox;
+
+    @FXML
+    private CheckBox DatVeThamQuanCheckBox;
+
+    @FXML
+    private CheckBox DatHuongDanVienCheckBox;
+
+    @FXML
+    private Label TongTienLabel;
+
+    @FXML
+    private TextField TongTienTextField;
+
+    @FXML
+    private Button LuuButton;
+
+    @FXML
+    private Button HuyBotton;
+
+    @FXML
+    void BuffeeChayCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuffeeSangCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuffeeToiCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuffeeTruaCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ChamSocTocCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CocaCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ComboBuffeeCaNgayCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DatHuongDanVienCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DatVeThamQuanCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DatXeTaxiCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HoBoiCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HuyBottonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MassageCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void NuocSuoiCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PepsiCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PhongGameCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void QuayBarCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RuaXeCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SanBongCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SevenUpCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SpaCheckBoxListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TongTienTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void XongHoiCheckBoxListener(ActionEvent event) {
+
+    }
+
+
+}
