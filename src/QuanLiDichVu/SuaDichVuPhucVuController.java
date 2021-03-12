74442860b45c9679316f9e4f6c2b491f8fa9a37b
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-13 02:23:21 +0700

    Giao Diện Sửa Dịch Vụ Phục Vụ (File java + java Controller , fxml)

---
+++ src/QuanLiDichVu/SuaDichVuPhucVuController.java
@@ -0,0 +1,115 @@
+package QuanLiDichVu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SuaDichVuPhucVuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane SuaDichVuGiaiTriAnchorPane;
+
+    @FXML
+    private Label SuaDichVuPhucVuLabel;
+
+    @FXML
+    private Label MaDVLabel;
+
+    @FXML
+    private Label TenDVLabel;
+
+    @FXML
+    private Label SoLuongLabel;
+
+    @FXML
+    private Label GiaLabel;
+
+    @FXML
+    private Label TinhTrangLLabel;
+
+    @FXML
+    private TextField MaDVTextField;
+
+    @FXML
+    private TextField TenDVTextField;
+
+    @FXML
+    private TextField SoLuongTextField;
+
+    @FXML
+    private TextField GiaTextField;
+
+    @FXML
+    private TextField TinhTrangTextField;
+
+    @FXML
+    private Button LuuButton;
+
+    @FXML
+    private Button HuyButton;
+
+    @FXML
+    void GiaTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HuyButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MaDVTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SoLuongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TenDVTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TinhTrangTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert SuaDichVuGiaiTriAnchorPane != null : "fx:id=\"SuaDichVuGiaiTriAnchorPane\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert SuaDichVuPhucVuLabel != null : "fx:id=\"SuaDichVuPhucVuLabel\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert MaDVLabel != null : "fx:id=\"MaDVLabel\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert TenDVLabel != null : "fx:id=\"TenDVLabel\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert SoLuongLabel != null : "fx:id=\"SoLuongLabel\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert GiaLabel != null : "fx:id=\"GiaLabel\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert TinhTrangLLabel != null : "fx:id=\"TinhTrangLLabel\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert MaDVTextField != null : "fx:id=\"MaDVTextField\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert TenDVTextField != null : "fx:id=\"TenDVTextField\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert SoLuongTextField != null : "fx:id=\"SoLuongTextField\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert GiaTextField != null : "fx:id=\"GiaTextField\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert TinhTrangTextField != null : "fx:id=\"TinhTrangTextField\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert LuuButton != null : "fx:id=\"LuuButton\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+        assert HuyButton != null : "fx:id=\"HuyButton\" was not injected: check your FXML file 'SuaDichVuPhucVuController.fxml'.";
+
+    }
+}
