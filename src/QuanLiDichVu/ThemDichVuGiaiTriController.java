fb4864f17d8b17ffb7427acf4d80f30d979404ee
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-13 01:52:59 +0700

    Giao Diện Thêm Dịch Vụ Giải trí (File java + java Controller, fxml)

---
+++ src/QuanLiDichVu/ThemDichVuGiaiTriController.java
@@ -0,0 +1,94 @@
+package QuanLiDichVu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ThemDichVuGiaiTriController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label ThemDichVuGIaiTriLabel;
+
+    @FXML
+    private Label MaDVAnUongLabel;
+
+    @FXML
+    private Label TenDVAnUongLabel;
+
+    @FXML
+    private Label KhungGioLabel;
+
+    @FXML
+    private Label GiaLabel;
+
+    @FXML
+    private Label TinhTrangLLabel;
+
+    @FXML
+    private TextField MaDVAnUongTextField;
+
+    @FXML
+    private TextField TenDVAnUongTextField;
+
+    @FXML
+    private TextField KhungGioTextField;
+
+    @FXML
+    private TextField GiaTextField;
+
+    @FXML
+    private TextField TinhTrangTextField;
+
+    @FXML
+    private Button LuuButton;
+
+    @FXML
+    private Button HuyButton;
+
+    @FXML
+    void GiaTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HuyButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KhungGioTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LuuButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MaDVAnUongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TenDVAnUongTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TinhTrangTextFieldListener(ActionEvent event) {
+
+    }
+
+   
+}
