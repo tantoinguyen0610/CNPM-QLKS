ba40854c068ecc2aa2fec5054c5c018b224b966c
undead4569 <57521804+undead4569@users.noreply.github.com>
2021-03-14 15:59:08 +0700

    Update NhanVien.java

--- src/NhanVien/NhanVien.java
+++ src/NhanVien/NhanVien.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class NhanVien extends Application {
 
 	public void start(Stage stage) throws Exception
