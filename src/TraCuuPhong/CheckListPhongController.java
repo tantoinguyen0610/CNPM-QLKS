217ea67d497a2e372a1c9c884d8a69cecda778e9
builehongthang <blht01218332420@gmail.com>
2021-03-14 20:40:12 +0700

    Xử lý giao diện Check-List Phòng

---
+++ src/TraCuuPhong/CheckListPhongController.java
@@ -0,0 +1,65 @@
+package TraCuuPhong;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class CheckListPhongController {
+
+    @FXML
+    private AnchorPane Ap;
+
+    @FXML
+    private CheckBox cb2;
+
+    @FXML
+    private CheckBox cb8;
+
+    @FXML
+    private CheckBox cb12;
+
+    @FXML
+    private CheckBox cb1;
+
+    @FXML
+    private CheckBox cb3;
+
+    @FXML
+    private CheckBox cb4;
+
+    @FXML
+    private CheckBox cb13;
+
+    @FXML
+    private CheckBox cb5;
+
+    @FXML
+    private CheckBox cb7;
+
+    @FXML
+    private CheckBox cb6;
+
+    @FXML
+    private CheckBox cb10;
+
+    @FXML
+    private CheckBox cb11;
+
+    @FXML
+    private CheckBox cb9;
+
+    @FXML
+    private CheckBox cb14;
+
+    @FXML
+    private Label lbBangchecklist;
+
+    @FXML
+    private Button btXacnhan;
+
+    @FXML
+    private Button btHuy;
+
+}
