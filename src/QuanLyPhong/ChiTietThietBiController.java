8d4fba11a984fe76a500fe864d3703d3492fb987
builehongthang <blht01218332420@gmail.com>
2021-03-14 20:54:05 +0700

    Xử lý chi tiết thiết bị

---
+++ src/QuanLyPhong/ChiTietThietBiController.java
@@ -0,0 +1,50 @@
+package QuanLyPhong;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ChiTietThietBiController {
+
+    @FXML
+    private AnchorPane Ap;
+
+    @FXML
+    private CheckBox cb5;
+
+    @FXML
+    private CheckBox cb1;
+
+    @FXML
+    private CheckBox cb2;
+
+    @FXML
+    private CheckBox cb8;
+
+    @FXML
+    private CheckBox cb6;
+
+    @FXML
+    private CheckBox cb3;
+
+    @FXML
+    private CheckBox cb4;
+
+    @FXML
+    private CheckBox cb7;
+
+    @FXML
+    private Button btCapnhat;
+
+    @FXML
+    private Button btLuu;
+
+    @FXML
+    private Button btHuy;
+
+    @FXML
+    private Label lbThietbiphong;
+
+}
