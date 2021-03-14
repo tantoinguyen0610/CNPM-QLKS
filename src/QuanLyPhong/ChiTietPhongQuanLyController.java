598e4cd1b41ba798d0a0f9c29cb5417d03a60777
builehongthang <blht01218332420@gmail.com>
2021-03-14 20:53:23 +0700

    Xử lý chi tiết phòng quản lý

---
+++ src/QuanLyPhong/ChiTietPhongQuanLyController.java
@@ -0,0 +1,56 @@
+package QuanLyPhong;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ChiTietPhongQuanLyController {
+
+    @FXML
+    private AnchorPane Ap;
+
+    @FXML
+    private Label lbThietbi;
+
+    @FXML
+    private Label lbKiemtradinhki;
+
+    @FXML
+    private Label lbTrangthai;
+
+    @FXML
+    private Label lbPhong;
+
+    @FXML
+    private ComboBox<?> cbSonguoichophep;
+
+    @FXML
+    private ComboBox<?> cbTrangthai;
+
+    @FXML
+    private Button btChecklist;
+
+    @FXML
+    private Button btThoat;
+
+    @FXML
+    private Button btKhoaphong;
+
+    @FXML
+    private Button btCapnhat;
+
+    @FXML
+    private Label lbSonguoichophep;
+
+    @FXML
+    private Button btThietbi;
+
+    @FXML
+    private Button btKiemtradinhki;
+
+    @FXML
+    private Button btChinhsua;
+
+}
