6538ab41e015c86fe0967113b8efd9728e418772
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-15 16:52:04 +0700

    Giao diện Login

--- src/LogIn/Login.java
+++ src/LogIn/Login.java
@@ -1,33 +1,32 @@
 package LogIn;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Login extends Application {
+
+	public void start(Stage stage) throws Exception
+	 {
+	 // Load the FXML file.
+	 Parent parent = FXMLLoader.load(
+	 getClass().getResource("GiaoDienLogIn.fxml"));
 	
-	private static Stage stg;
+	 // Build the scene graph.
+	 Scene scene = new Scene(parent);
 	
+	 // Display our window, using the scene graph.
+	 stage.setTitle("QLKS");
+	stage.setScene(scene);
+	 stage.show();
+	 }
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		stg = primaryStage;
-		primaryStage.setResizable(false);
-		Parent root = FXMLLoader.load(getClass().getResource("GiaoDienLogIn.fxml"));
-		primaryStage.setTitle("Hello");
-		primaryStage.setScene(new Scene(root, 600, 400));
-		primaryStage.show();
-	}
-	
-	public void changeScene(String fxml) throws IOException {
-		Parent pane = FXMLLoader.load(getClass().getResource(fxml));
-		stg.getScene().setRoot(pane);
-	}
-	
-	public static void main(String[] args) { launch(args); }
+	 public static void main(String[] args)
+ {
+	 // Launch the application.
+	 launch(args);
+	 }
+
 }
