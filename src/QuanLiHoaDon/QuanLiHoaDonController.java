a3397f1d4f990f1e83007edebda455c5d1b73eea
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-15 21:16:55 +0700

    Thêm Xử lí vào controller

--- src/QuanLiHoaDon/QuanLiHoaDonController.java
+++ src/QuanLiHoaDon/QuanLiHoaDonController.java
@@ -1,9 +1,13 @@
 package QuanLiHoaDon;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -12,6 +16,7 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class QuanLiHoaDonController {
 
@@ -175,72 +180,84 @@ public class QuanLiHoaDonController {
     private Button DuyetThanhToanHoaDonNCPButton;
 
     @FXML
-    void DenNgayDatePickerListener(ActionEvent event) {
+    void DenNgayDatePickerListener(ActionEvent event) throws IOException {
+    	
+    }
+
+    @FXML
+    void DenNgayNCPDatePickerListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void DenNgayNCPDatePickerListener(ActionEvent event) {
+    void DenNgaySCTBDatePickerListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void DenNgaySCTBDatePickerListener(ActionEvent event) {
+    void DuyetThanhToanHoaDonNCPButtonListener(ActionEvent event) throws IOException {
+    	
+    }
+
+    @FXML
+    void DuyetThanhToanHoaDonSCTBButtonListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void DuyetThanhToanHoaDonNCPButtonListener(ActionEvent event) {
+    void TuNgayDatePickerListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void DuyetThanhToanHoaDonSCTBButtonListener(ActionEvent event) {
+    void TuNgayNCPDatePickerListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void TuNgayDatePickerListener(ActionEvent event) {
+    void TuNgaySCTBDatePickerListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void TuNgayNCPDatePickerListener(ActionEvent event) {
+    void XemChiTietHDTTPButtonListener(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("ChiTietHoaDonThanhToanPhongController.fxml"));
+    	Scene scene = new Scene(root);
+    	Stage stage = new Stage();
+    	stage.setScene(scene);
+    	stage.show();
+    }
+
+    @FXML
+    void XemHoaDonNCPButtonListener(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("ChiTietHoaDonNangCapPhongController.fxml"));
+    	Scene scene = new Scene(root);
+    	Stage stage = new Stage();
+    	stage.setScene(scene);
+    	stage.show();
+    }
+
+    @FXML
+    void XemHoaDonSCTBButtonListener(ActionEvent event) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("ChiTietHoaDonSuaChuaThietBiController.fxml"));
+    	Scene scene = new Scene(root);
+    	Stage stage = new Stage();
+    	stage.setScene(scene);
+    	stage.show();
+    }
+
+    @FXML
+    void XoaHDTTPButtonListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void TuNgaySCTBDatePickerListener(ActionEvent event) {
+    void XoaHoaDonNCPButtonListener(ActionEvent event) throws IOException {
 
     }
 
     @FXML
-    void XemChiTietHDTTPButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void XemHoaDonNCPButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void XemHoaDonSCTBButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void XoaHDTTPButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void XoaHoaDonNCPButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void XoaHoaDonSCTBButtonListener(ActionEvent event) {
+    void XoaHoaDonSCTBButtonListener(ActionEvent event) throws IOException {
 
     }
 
