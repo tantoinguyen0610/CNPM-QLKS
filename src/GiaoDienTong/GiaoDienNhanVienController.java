ce14451cf19fccb21020f6f542ebb93dc94b9b7c
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-21 23:02:31 +0700

    Chỉnh Sửa

--- src/GiaoDienTong/GiaoDienNhanVienController.java
+++ src/GiaoDienTong/GiaoDienNhanVienController.java
@@ -1,10 +1,15 @@
 package GiaoDienTong;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class GiaoDienNhanVienController {
 
@@ -27,8 +32,8 @@ public class GiaoDienNhanVienController {
     private Button QuanLiKhachHangButton;
 
     @FXML
-    void Check_inButtonListener(ActionEvent event) {
-
+    void Check_inButtonListener(ActionEvent event) throws IOException {
+   
     }
 
     @FXML
