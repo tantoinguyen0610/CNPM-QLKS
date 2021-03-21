d62d14bdff937ccf695f14b2889fb2b315200bbc
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-21 22:58:42 +0700

    Giao Diện Nhân Viên

--- src/GiaoDienTong/GiaoDienNhanVien.java
+++ src/GiaoDienTong/GiaoDienNhanVien.java
@@ -1,14 +1,32 @@
 package GiaoDienTong;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GiaoDienNhanVien extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
+	public void start(Stage stage) throws Exception
+	 {
+	 // Load the FXML file.
+	 Parent parent = FXMLLoader.load(
+	 getClass().getResource("GiaoDienNhanVienController.fxml"));
+	
+	 // Build the scene graph.
+	 Scene scene = new Scene(parent);
+	
+	 // Display our window, using the scene graph.
+	 stage.setTitle("Giao dien Check-in");
+	stage.setScene(scene);
+	 stage.show();
+	 }
+	
+	 public static void main(String[] args)
+ {
+	 // Launch the application..
+	 launch(args);
+	 }
 
 }
---
+++ src/GiaoDienTong/GiaoDienNhanVienController.java
@@ -0,0 +1,45 @@
+package GiaoDienTong;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GiaoDienNhanVienController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button Check_inButton;
+
+    @FXML
+    private Button Check_outButton;
+
+    @FXML
+    private Button TraCuuPhongButton;
+
+    @FXML
+    private Button QuanLiKhachHangButton;
+
+    @FXML
+    void Check_inButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Check_outButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void QuanLiKhachHangButtonListener(ActionEvent event) {
+
+    }
+
+   
+}
