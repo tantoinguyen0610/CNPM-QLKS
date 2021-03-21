aabb3b12209e6026356597e8ab8ec03249183344
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-21 23:09:44 +0700

    Giao Diên Quản Lí

---
+++ src/GiaoDienTong/GiaoDienQuanLiController.java
@@ -0,0 +1,42 @@
+package GiaoDienTong;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GiaoDienQuanLiController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button QLPButton;
+
+    @FXML
+    private Button QLNVButton;
+
+    @FXML
+    private Button QLDVButton;
+
+    @FXML
+    void QLDVButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void QLNVButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void QLPButtonListener(ActionEvent event) {
+
+    }
+
+   
+}
