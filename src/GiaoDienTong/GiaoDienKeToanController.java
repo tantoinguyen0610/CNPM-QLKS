b17199c1df9f5a7882f5b7f06cda717cf4cdd508
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-21 23:16:14 +0700

    Giao Diện Kế Toán

---
+++ src/GiaoDienTong/GiaoDienKeToanController.java
@@ -0,0 +1,34 @@
+package GiaoDienTong;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GiaoDienKeToanController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ThongKeButton;
+
+    @FXML
+    private Button QLHDButton;
+
+    @FXML
+    void QLHDButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ThongKeButtonListener(ActionEvent event) {
+
+    }
+
+    
+}
