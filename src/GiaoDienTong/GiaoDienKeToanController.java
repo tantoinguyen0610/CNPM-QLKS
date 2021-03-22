69cda4050d1c7e0079d2198a2ac814ae1df4aed3
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-03-22 17:11:12 +0700

    Chinh Sua

--- src/GiaoDienTong/GiaoDienKeToanController.java
+++ src/GiaoDienTong/GiaoDienKeToanController.java
@@ -4,8 +4,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
+import javafx.stage.Stage;
+import HoaDon.QuanLiHoaDonController;
 public class GiaoDienKeToanController {
 
     @FXML
@@ -21,8 +25,16 @@ public class GiaoDienKeToanController {
     private Button QLHDButton;
 
     @FXML
-    void QLHDButtonListener(ActionEvent event) {
-
+    void QLHDButtonListener(ActionEvent event) throws Exception {
+    	Parent parent = FXMLLoader.load(
+				 getClass().getClassLoader().getResource("HoaDon/QuanLiHoaDonController.fxml"));
+				 // Build the scene graph.
+				 Scene scene = new Scene(parent);
+				Stage stage = new Stage();
+				 // Display our window, using the scene graph.
+				 stage.setTitle("DatCoc");
+				stage.setScene(scene);
+				 stage.show();
     }
 
     @FXML
