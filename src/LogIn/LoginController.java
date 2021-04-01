07c7fdbe3ad43c1815082591645e37c2327b1be6
NWSZ3R0 <71622415+NWSZ3R0@users.noreply.github.com>
2021-04-01 20:28:00 +0700

    # WARNING: head commit changed in the meanUpdated Accounts

--- src/LogIn/LoginController.java
+++ src/LogIn/LoginController.java
@@ -1,9 +1,18 @@
 package LogIn;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -12,6 +21,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -58,8 +68,49 @@ public class LoginController {
     private Label wronglogin;
 
     @FXML
-    void Login_ActionListener(ActionEvent event) {
+    void Login_ActionListener(ActionEvent event) throws IOException {
+    	checkLogIn();
+    }
+    
+    
+    
+    
+    private void checkLogIn() throws IOException {
+		// TODO Auto-generated method stub
+		if (username_textField.getText().toString().equals("quanly") && password_textField.getText().toString().equals("QLKS")) {
+			Parent root = FXMLLoader.load(getClass().getResource("GiaoDienQuanLiController.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		}
+		else if (username_textField.getText().toString().equals("ketoan") && password_textField.getText().toString().equals("KTKS")) {
+			Parent root = FXMLLoader.load(getClass().getResource("GiaoDienKeToanController.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		}
+		else if (username_textField.getText().toString().equals("nhanvien") && password_textField.getText().toString().equals("NVKS")) {
+			Parent root = FXMLLoader.load(getClass().getResource("GiaoDienNhanVien.java.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
 
+
+
+
+	public void connectDB() {
+    	try {
+			final String DB_URL = "jdbc:mysql://localhost:8080/qlks_db";
+			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+    	}
+    	catch(Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			}
     }
 
    
