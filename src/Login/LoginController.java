6b00bee7730219cab437cefd6132a0d4f6b85032
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-04 12:34:24 +0700

    Login Chuẩn

---
+++ src/Login/LoginController.java
@@ -0,0 +1,57 @@
+package Login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane AnchorPane;
+
+    @FXML
+    private Label QLKSLabel;
+
+    @FXML
+    private Label UserLabel;
+
+    @FXML
+    private Label PasswordLabel;
+
+    @FXML
+    private TextField UserTextField;
+
+    @FXML
+    private TextField PasswordTextField;
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    void LoginButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PasswordTextFieldListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UserTextFieldListener(ActionEvent event) {
+
+    }
+
+    
+}
