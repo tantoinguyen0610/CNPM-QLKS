15cf3c30c68f59b636e57d5c2532e54d1cc2811a
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-06 22:29:27 +0700

    update code xử lí dịch vụ thư giản

--- src/QuanLiDichVu/ThemDichVuThuGianController.java
+++ src/QuanLiDichVu/ThemDichVuThuGianController.java
@@ -1,16 +1,25 @@
 package QuanLiDichVu;
 
-import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class ThemDichVuThuGianController {
+public class ThemDichVuThuGianController implements Initializable {
 
     @FXML
     private ResourceBundle resources;
@@ -31,7 +40,7 @@ public class ThemDichVuThuGianController {
     private Label KhungGioDVTGLabel;
 
     @FXML
-    private Label SoLuongNguoiLabel;
+    private Label LoaiDVLabel;
 
     @FXML
     private Label GiaLabel;
@@ -49,7 +58,7 @@ public class ThemDichVuThuGianController {
     private TextField KhungGioDVTGTextField;
 
     @FXML
-    private TextField SoLuongNguoiTextField;
+    private TextField LoaiDVTextField;
 
     @FXML
     private TextField GiaTextField;
@@ -63,13 +72,18 @@ public class ThemDichVuThuGianController {
     @FXML
     private Button HuyButton;
 
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+    	autoTaoMADV();
+	}
+    
     @FXML
     void GiaTextFieldListener(ActionEvent event) {
 
     }
 
     @FXML
-    void HuyButtonListener(ActionEvent event) throws IOException {
+    void HuyButtonListener(ActionEvent event) {
     	Stage stage = (Stage) HuyButton.getScene().getWindow();
         // do what you have to do
         stage.close();
@@ -81,17 +95,69 @@ public class ThemDichVuThuGianController {
     }
 
     @FXML
-    void LuuButtonListener(ActionEvent event) {
+    void LoaiDVTextFieldListener(ActionEvent event) {
 
     }
 
+    @FXML
+    void LuuButtonListener(ActionEvent event) {
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+			String query = "insert into `dv`(MA_DV,TENDV,LOAIDV,KHUNGGIO,GIA,TINHTRANG) VALUES(?,?,?,?,?,?)";
+			PreparedStatement pst = conn.prepareStatement(query);
+			pst.setString(1,MaDVAnUongTextField.getText());
+			pst.setString(2,TenDVAnUongTextField.getText());
+			pst.setString(3,LoaiDVTextField.getText());
+			pst.setString(4,KhungGioDVTGTextField.getText());		
+			pst.setString(5,GiaTextField.getText());
+			pst.setString(6,TinhTrangTextField.getText());
+			pst.executeUpdate();
+			JOptionPane.showMessageDialog(null, "Thêm Thành Công!"); 
+			//UpdateTable();
+		}
+		catch(Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+    	autoTaoMADV();
+    	Stage stage = (Stage) LuuButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+    
+    public void autoTaoMADV() {
+		try {
+			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+			Statement s= conn.createStatement();
+			
+			ResultSet rs = s.executeQuery("select Max(Ma_DV) from dv");
+			rs.next();
+			rs.getString("Max(Ma_DV)");
+			
+			if(rs.getString("Max(Ma_DV)")== null)
+			{
+				MaDVAnUongTextField.setText("1");
+			}
+			else
+			{
+				int madv = Integer.parseInt(rs.getString("Max(Ma_DV)"));
+				madv++;
+				MaDVAnUongTextField.setText(String.format("%d",madv));
+			}
+			
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+    
+    
     @FXML
     void MaDVAnUongTextFieldListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SoLuongNguoiTextFieldListener(ActionEvent event) {
 
     }
 
