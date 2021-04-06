fa7a9e3a47968cc53dfd0fc4f023b237b7b78378
NWSZ3R0 <71622415+NWSZ3R0@users.noreply.github.com>
2021-04-06 18:52:43 +0700

    Update Login with database

--- src/Login/LoginController.java
+++ src/Login/LoginController.java
@@ -1,15 +1,27 @@
 package Login;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     @FXML
     private ResourceBundle resources;
@@ -37,10 +49,42 @@ public class LoginController {
 
     @FXML
     private Button LoginButton;
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+    
+    public void HienThiQuanLi() throws Exception {
+    	Parent parent = FXMLLoader.load(
+				getClass().getResource("GiaoDienTong/GiaoDienQuanLiController.fxml"));
+				Scene scene = new Scene(parent);
+				Stage stage = new Stage();
+				stage.setTitle("Giao Dien Quan Li");
+				stage.setScene(scene);
+				stage.show();
+    }
 
     @FXML
-    void LoginButtonListener(ActionEvent event) {
-
+    void LoginButtonListener(ActionEvent event) throws Exception {
+    	try {
+			 final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+			 Connection conn = DriverManager.getConnection(DB_URL,"root","");
+			 String query = "select * from taikhoan where username=? and psw=?";
+			 PreparedStatement pst = conn.prepareStatement(query);
+			 pst.setString(1,UserTextField.getText());
+			 pst.setString(2, PasswordTextField.getText());
+			 pst.executeQuery();
+			 JOptionPane.showMessageDialog(null, "Dang zo"); 
+			 HienThiQuanLi();
+			 
+   }
+    	catch(SQLException e)
+    	{
+    		JOptionPane.showMessageDialog(null, e);
+    	}
+    	
     }
 
     @FXML
@@ -50,8 +94,11 @@ public class LoginController {
 
     @FXML
     void UserTextFieldListener(ActionEvent event) {
+    		
 
-    }
-
+   }
+    
+    
     
 }
+
