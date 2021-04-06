0307b81561ca1599a922277743cad443f7407d96
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-06 21:23:38 +0700

    Update code xử lí thêm dịch vụ ăn uống

--- src/QuanLiDichVu/ThemDichVuAnUongController.java
+++ src/QuanLiDichVu/ThemDichVuAnUongController.java
@@ -2,15 +2,25 @@ package QuanLiDichVu;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class ThemDichVuAnUongController {
+public class ThemDichVuAnUongController implements Initializable {
 
     @FXML
     private ResourceBundle resources;
@@ -63,6 +73,11 @@ public class ThemDichVuAnUongController {
     @FXML
     private Button HuyButton;
 
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+    	autoTaoMADV();
+	}
+    
     @FXML
     void GiaTextFieldListener(ActionEvent event) {
 
@@ -82,8 +97,58 @@ public class ThemDichVuAnUongController {
 
     @FXML
     void LuuButtonListener(ActionEvent event) {
-
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+			String query = "insert into `dv`(MA_DV,TENDV,LOAIDV,SOLUONG,GIA,TINHTRANG) VALUES(?,?,?,?,?,?)";
+			PreparedStatement pst = conn.prepareStatement(query);
+			pst.setString(1,MaDVAnUongTextField.getText());
+			pst.setString(2,TenDVAnUongTextField.getText());
+			pst.setString(3,LoaiDVAnUongTextField.getText());
+			pst.setString(4,SoLuongTextField.getText());		
+			pst.setString(5,GiaTextField.getText());
+			pst.setString(6,TinhTrangTextField.getText());
+			pst.executeUpdate();
+			JOptionPane.showMessageDialog(null, "Thêm Thành Công!"); 
+			//UpdateTable();
+		}
+		catch(Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+    	autoTaoMADV();
+    	Stage stage = (Stage) LuuButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
+    
+    public void autoTaoMADV() {
+		try {
+			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+			Statement s= conn.createStatement();
+			
+			ResultSet rs = s.executeQuery("select Max(Ma_DV) from dv");
+			rs.next();
+			rs.getString("Max(Ma_DV)");
+			
+			if(rs.getString("Max(Ma_DV)")== null)
+			{
+				MaDVAnUongTextField.setText("1");
+			}
+			else
+			{
+				int madv = Integer.parseInt(rs.getString("Max(Ma_DV)"));
+				madv++;
+				MaDVAnUongTextField.setText(String.format("%d",madv));
+			}
+			
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
 
     @FXML
     void MaDVAnUongTextFieldListener(ActionEvent event) {
