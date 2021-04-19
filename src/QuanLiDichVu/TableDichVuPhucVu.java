de60bece47268bad9a9d2db6a0b4bed8d5d8e0a8
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-19 23:28:34 +0700

    Thêm table Dịch Vụ Phục Vụ

---
+++ src/QuanLiDichVu/TableDichVuPhucVu.java
@@ -0,0 +1,65 @@
+package QuanLiDichVu;
+
+public class TableDichVuPhucVu extends Object {
+	String MA_DV,TENDV,SOLUONG,LOAIDV,TRANGTHAI,GIA;
+		
+	public TableDichVuPhucVu(String MA_DV, String TENDV, 
+			String SOLUONG,String LOAIDV, 
+			 String TRANGTHAI, String GIA) {
+		this.MA_DV=MA_DV;
+		this.TENDV=TENDV;
+		this.SOLUONG=SOLUONG;
+		this.LOAIDV=LOAIDV;
+		this.TRANGTHAI=TRANGTHAI;
+		this.GIA=GIA;
+	}
+
+	public String getMA_DV() {
+		return MA_DV;
+	}
+
+	public void setMA_DV(String mA_DV) {
+		MA_DV = mA_DV;
+	}
+
+	public String getTENDV() {
+		return TENDV;
+	}
+
+	public void setTENDV(String tENDV) {
+		TENDV = tENDV;
+	}
+
+	public String getSOLUONG() {
+		return SOLUONG;
+	}
+
+	public void setSOLUONG(String sOLUONG) {
+		SOLUONG = sOLUONG;
+	}
+
+	public String getLOAIDV() {
+		return LOAIDV;
+	}
+
+	public void setLOAIDV(String lOAIDV) {
+		LOAIDV = lOAIDV;
+	}
+
+	public String getTRANGTHAI() {
+		return TRANGTHAI;
+	}
+
+	public void setTRANGTHAI(String tRANGTHAI) {
+		TRANGTHAI = tRANGTHAI;
+	}
+
+	public String getGIA() {
+		return GIA;
+	}
+
+	public void setGIA(String gIA) {
+		GIA = gIA;
+	}
+	
+}
