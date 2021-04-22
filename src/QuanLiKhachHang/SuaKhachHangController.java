304e4fe9a7489654ecd4772b6813d5605330d334
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-22 20:21:43 +0700

    Cập nhật giao diện Khách Hàng

--- src/QuanLiKhachHang/SuaKhachHangController.java
+++ src/QuanLiKhachHang/SuaKhachHangController.java
@@ -2,7 +2,12 @@ package QuanLiKhachHang;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
 
 import Check_in.ModelTable;
 import javafx.event.ActionEvent;
@@ -50,10 +55,6 @@ public class SuaKhachHangController {
 
     @FXML
     private Label DiaChiLabel;
-
-
-    @FXML
-    private Label SoNgayOLabel;
 
     @FXML
     private TextField MaKHTextField;
@@ -115,7 +116,30 @@ public class SuaKhachHangController {
 
     @FXML
     void LuuButtonListener(ActionEvent event) {
-    	
+    	int response=	JOptionPane.showConfirmDialog(null, "Bạn chắc chắn muốn lưu cập nhật?", "xác nhận", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+     	if(response == JOptionPane.YES_OPTION) {
+     		try {
+    			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+    			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+    			String value0 = MaKHTextField.getText();
+    			String value1 = TenKhTextField.getText();
+    			String value2 =NgaySinhTextField.getText();
+    			String value3 =GTTextField.getText();
+    			String value4 =DiaChiTextField.getText();
+    			String value5 =QuocTichTextField.getText();
+    			String value6 =SDTTextField.getText();
+    			String value7 =CMNDTextField.getText();
+    			String sql = "update khachhang set MAKH= '"+value0+"',TENKH='"+value1+"',NGAYSINH='"+value2+"',GIOITINH='"+value3+"',DIACHI='"+value4+"',QUOCTICH='"+value5+"',SDT='"+value6+"',CMND='"+value7+"'";
+    			PreparedStatement pst = conn.prepareStatement(sql);	
+    			pst.execute();
+    			JOptionPane.showMessageDialog(null, "Cập nhật thành công!");
+    		}catch(Exception e) {
+    			e.printStackTrace();
+    		}
+     	}
+     	else if(response == JOptionPane.NO_OPTION){
+     		
+     	}
     }
 
     @FXML
@@ -138,10 +162,6 @@ public class SuaKhachHangController {
 
     }
 
-    @FXML
-    void SoNgayOTextFieldListener(ActionEvent event) {
-
-    }
     
     public void setKhachHang(TableKhachHang khachhang) {
     	MaKHTextField.setText(String.valueOf(khachhang.getMAKH()));
