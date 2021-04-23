747c6f76f5a5bf9008af82846b28aca51a5046f6
undead4569 <pth.a3.30.hungphu@gmail.com>
2021-04-23 14:41:51 +0700

    Cap nhat QLP

--- src/QuanLyPhong/Phong102Controller.java
+++ src/QuanLyPhong/Phong102Controller.java
@@ -27,61 +27,61 @@ import javafx.stage.Stage;
 
 public class Phong102Controller implements Initializable {
 
-    @FXML
-    private AnchorPane Ap;
+	 @FXML
+	    private AnchorPane Ap;
 
-    @FXML
-    private Label lbThietbi;
+	    @FXML
+	    private Label lbThietbi;
 
-    @FXML
-    private Label lbTrangthai;
+	    @FXML
+	    private Label lbTrangthai;
 
-    @FXML
-    private Label lbPhong;
+	    @FXML
+	    private Label lbPhong;
 
-    @FXML
-    private ComboBox<String> cbTrangthai;
+	    @FXML
+	    private ComboBox<String> cbTrangthai1;
 
-    @FXML
-    private Label lbSonguoichophep;
+	    @FXML
+	    private Label lbSonguoichophep;
 
-    @FXML
-    private Button btThietbi;
+	    @FXML
+	    private Button btThietbi1;
 
+	    @FXML
+	    private Label SoPhong_Label;
 
-    @FXML
-    private Label SoPhong_Label;
+	    @FXML
+	    private TextField SoPhong_textField1;
 
-    @FXML
-    private TextField SoPhong_textField;
+	    @FXML
+	    private Button btLuu;
 
-    @FXML
-    private Button btLuu;
+	    @FXML
+	    private Button btChinhsua;
 
-    @FXML
-    private Button btChinhsua;
+	    @FXML
+	    private Button btThoat;
 
-    @FXML
-    private Button btThoat;
+	    @FXML
+	    private TextField cbSonguoichophep1;
 
-    @FXML
-    private TextField cbSonguoichophep;
     
     ObservableList<String> listtrangthai = FXCollections.observableArrayList("Trống","Đang sử dụng", "Sắp trả", "Đặt trước","Thanh toán");
     
     @Override
 	public void initialize(java.net.URL arg0, ResourceBundle arg1) {
     	
-         cbTrangthai.setItems(listtrangthai);
-         cbSonguoichophep.setDisable(true);
-         cbTrangthai.setDisable(true);
-         btThietbi.setDisable(true);
+         cbTrangthai1.setItems(listtrangthai);
+         cbSonguoichophep1.setDisable(true);
+         cbTrangthai1.setDisable(true);
+         btThietbi1.setDisable(true);
          btLuu.setDisable(true);
-         HienTextField();
+         HienTextField1();
 		
     }
     
-    public void HienTextField() {
+    public void HienTextField1() {
     	try {
 			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
 			Connection conn = DriverManager.getConnection(DB_URL,"root","");
@@ -91,9 +91,9 @@ public class Phong102Controller implements Initializable {
 			 PreparedStatement pst1 = conn.prepareStatement(query1);
 			 ResultSet rs1 = pst1.executeQuery();
 			 while(rs1.next()) {
-				 SoPhong_textField.setText(rs1.getString("TEN_PHONG"));
-				 cbSonguoichophep.setText(rs1.getString("SONGUOIO"));
-				 cbTrangthai.setValue(rs1.getString("TINHTRANG"));
+				 SoPhong_textField1.setText(rs1.getString("TEN_PHONG"));
+				 cbSonguoichophep1.setText(rs1.getString("SONGUOIO"));
+				 cbTrangthai1.setValue(rs1.getString("TINHTRANG"));
 			 }
 		}
 			catch(Exception e) {
@@ -107,9 +107,9 @@ public class Phong102Controller implements Initializable {
     	try {						
    		 final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
 			Connection conn = DriverManager.getConnection(DB_URL,"root","");
-			String value1 = cbTrangthai.getValue().toString();
-			String value2 = cbSonguoichophep.getText();
-			String value3 = SoPhong_textField.getText();
+			String value1 = cbTrangthai1.getValue().toString();
+			String value2 = cbSonguoichophep1.getText();
+			String value3 = SoPhong_textField1.getText();
 			String query1 = "UPDATE phong SET TINHTRANG = '"+value1+"', SONGUOIO = '"+value2+"' WHERE TEN_PHONG='"+value3+"' ";
 			 PreparedStatement pst1 = conn.prepareStatement(query1);
 			 pst1.executeUpdate();
@@ -127,7 +127,7 @@ public class Phong102Controller implements Initializable {
     }
 
     @FXML
-    void btactionthietbi(ActionEvent event)throws IOException {
+    void btactionthietbi1(ActionEvent event)throws IOException {
     	Parent root = FXMLLoader.load(getClass().getResource("TB102.fxml"));
     	Scene scene = new Scene(root);
     	Stage stage = new Stage();   	
@@ -137,10 +137,10 @@ public class Phong102Controller implements Initializable {
 
     @FXML
     void btchinhsua(ActionEvent event) {
-    	cbSonguoichophep.setDisable(false);
-        cbTrangthai.setDisable(false);
+    	cbSonguoichophep1.setDisable(false);
+        cbTrangthai1.setDisable(false);
         btChinhsua.setDisable(true);
-        btThietbi.setDisable(false);
+        btThietbi1.setDisable(false);
         btLuu.setDisable(false);
     }
 
