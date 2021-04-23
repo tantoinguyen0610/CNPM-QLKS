cd4d2b350d2778edba187c09f5ab56e0a9773c3c
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-23 20:26:13 +0700

    chỉnh sửa giao diện dịch vụ phục vụ Thêm code xử lí sửa dịch vụ phục vụ

--- src/QuanLiDichVu/SuaDichVuPhucVuController.java
+++ src/QuanLiDichVu/SuaDichVuPhucVuController.java
@@ -2,7 +2,13 @@ package QuanLiDichVu;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -63,6 +69,12 @@ public class SuaDichVuPhucVuController {
     
     @FXML
     private Button SuaButton;
+    
+    @FXML
+    private Label LoaiDVLabel;
+
+    @FXML
+    private TextField LoaiDVTextField;
 
     @FXML
     void GiaTextFieldListener(ActionEvent event) {
@@ -78,7 +90,32 @@ public class SuaDichVuPhucVuController {
 
     @FXML
     void LuuButtonListener(ActionEvent event) {
-
+    	int response=	JOptionPane.showConfirmDialog(null, "Bạn chắc chắn muốn lưu cập nhật?", "xác nhận", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+     	if(response == JOptionPane.YES_OPTION) {
+     		try {
+    			final String DB_URL = "jdbc:mysql://localhost:3306/qlks_db";
+    			Connection conn = DriverManager.getConnection(DB_URL,"root","");
+    			String value1 = MaDVTextField.getText();
+    			String value2 = TenDVTextField.getText();
+    			String value3 =	LoaiDVTextField.getText();
+    			String value4 = SoLuongTextField.getText();
+    			String value5 = GiaTextField.getText();
+    			String value6 = TinhTrangTextField.getText();
+    			String sql = "update dv set MA_DV= '"+value1+"',TENDV='"+value2+"',LOAIDV='"+value3+"',SOLUONG='"+value4+"',GIA='"+value5+"',TINHTRANG='"+value6+"' where MA_DV='"+value1+"'";
+    			PreparedStatement pst = conn.prepareStatement(sql);	
+    			pst.execute();
+    			JOptionPane.showMessageDialog(null, "Cập nhật thành công!");
+    		}catch(Exception e) {
+    			e.printStackTrace();
+    			
+    		}
+     		Stage stage = (Stage) LuuButton.getScene().getWindow();
+	        // do what you have to do
+	        stage.close();
+     	}
+     	else if(response == JOptionPane.NO_OPTION){
+     		
+     	}
     }
 
     @FXML
@@ -98,9 +135,20 @@ public class SuaDichVuPhucVuController {
     	SoLuongTextField.setDisable(false);
     	GiaTextField.setDisable(false);
     	TinhTrangTextField.setDisable(false);
+    	LoaiDVTextField.setDisable(true);
     	LuuButton.setDisable(false);
     	HuyButton.setDisable(false);
     	SuaButton.setDisable(true);
+    }
+    
+    public void setDichVuPV(TableDichVuPhucVu dichvuphucvu) {
+    	MaDVTextField.setText(String.valueOf(dichvuphucvu.getMA_DV()));
+    	TenDVTextField.setText(String.valueOf(dichvuphucvu.getTENDV()));
+    	SoLuongTextField.setText(String.valueOf(dichvuphucvu.getLOAIDV()));
+    	LoaiDVTextField.setText(String.valueOf(dichvuphucvu.getSOLUONG()));
+    	GiaTextField.setText(String.valueOf(dichvuphucvu.getGIA()));
+    	TinhTrangTextField.setText(String.valueOf(dichvuphucvu.getTINHTRANG()));
+    	
     }
 
     @FXML
