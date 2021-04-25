2fefcf43e2fbf9b341c9287f826bba5b1eb5ec08
undead4569 <pth.a3.30.hungphu@gmail.com>
2021-04-25 22:11:27 +0700

    Chỉnh sửa

--- src/Check_out/CheckOut.java
+++ src/Check_out/CheckOut.java
@@ -10,14 +10,9 @@ public class CheckOut extends Application {
 
 	public void start(Stage stage) throws Exception
 	 {
-	 // Load the FXML file.
 	 Parent parent = FXMLLoader.load(
 	 getClass().getResource("GiaoDien_Check_Out.fxml"));
-	
-	 // Build the scene graph.
 	 Scene scene = new Scene(parent);
-	
-	 // Display our window, using the scene graph.
 	 stage.setTitle("Giao dien Check-out");
 	stage.setScene(scene);
 	 stage.show();
