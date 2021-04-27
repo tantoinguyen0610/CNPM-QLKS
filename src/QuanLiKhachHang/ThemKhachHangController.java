9963929b9d82e5ca3d27473173fdb83ac80b002d
tantoinguyen0610 <72715687+tantoinguyen0610@users.noreply.github.com>
2021-04-27 22:07:50 +0700

    Chỉnh Sửa Giao Diện and Code xử lí

--- src/QuanLiKhachHang/ThemKhachHangController.java
+++ src/QuanLiKhachHang/ThemKhachHangController.java
@@ -11,6 +11,8 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 
@@ -69,9 +71,6 @@ public class ThemKhachHangController implements Initializable {
     private TextField TenKhTextField;
 
     @FXML
-    private TextField GTTextField;
-
-    @FXML
     private TextField SDTTextField;
 
     @FXML
@@ -79,9 +78,6 @@ public class ThemKhachHangController implements Initializable {
 
     @FXML
     private TextField QuocTichTextField;
-
-    @FXML
-    private TextField NgaySinhTextField;
 
     @FXML
     private TextField DiaChiTextField;
@@ -248,24 +244,32 @@ public class ThemKhachHangController implements Initializable {
 
     private boolean KiemTraSDT() {
 	
-	if(SDTTextField.getText().isEmpty())
-			
-		{
-		tb2Label.setText("Vui lòng nhập Số Điện Thoại ");
-		return false;
+    	Pattern p = Pattern.compile("[0-9]+");
+		Matcher m = p.matcher(SDTTextField.getText());
+		if(m.find() && m.group().equals(SDTTextField.getText())&& SDTTextField.getText().matches("\\d{10}|\\d{11}"))
+				{
+			tb2Label.setText("");
+			return true;
 		}
-	return true;
-}
+		else {
+			tb2Label.setText("Vui lòng điền số hợp lệ");
+			return false;
+		}
+	}
 
     private boolean KiemTraCMND() {
 	
-	if(CMNDTextField.getText().isEmpty())
-			
-		{
-		tb3Label.setText("Vui lòng nhập CMND ");
-		return false;
+    	Pattern p = Pattern.compile("[0-9]+");
+		Matcher m = p.matcher(CMNDTextField.getText());
+		if(m.find() && m.group().equals(CMNDTextField.getText())&& CMNDTextField.getText().matches("\\d{10}|\\d{11}"))
+				{
+			tb3Label.setText("");
+			return true;
 		}
-	return true;
+		else {
+			tb3Label.setText("Vui lòng điền số hợp lệ");
+			return false;
+		}
 }
 
     private boolean KiemTraQUOCTICH() {
